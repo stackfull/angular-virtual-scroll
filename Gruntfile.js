e73e5b12c76a8abedb512e11d617673a2ccf68da
Paul Thomas <paul@stackfull.com>
2013-03-16 21:44:26 +0000

    use files names without versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function( grunt ) {
       },
       dist: {
         src: DISTSOURCES,
-        dest: DISTDIR + '/<%= pkg.name %>-<%= pkg.version %>.js'
+        dest: DISTDIR + '/<%= pkg.name %>.js'
       }
     },
 
@@ -46,7 +46,7 @@ module.exports = function( grunt ) {
       },
       dist: {
         src: [ '<%= concat.dist.dest %>' ],
-        dest: DISTDIR + '/<%= pkg.name %>-<%= pkg.version %>.min.js'
+        dest: DISTDIR + '/<%= pkg.name %>.min.js'
 
       }
     },
@@ -197,5 +197,56 @@ module.exports = function( grunt ) {
 
   grunt.registerTask('demo', ['bowerful', 'connect:demo', 'watch']);
 
+  var shell = require('shelljs');
+  var semver = require('semver');
 
+  function run(cmd, msg){
+    shell.exec(cmd, {silent:true});
+    if( msg ){
+      grunt.log.ok(msg);
+    }
+  }
+
+  grunt.registerTask('release-prepare', 'Set up submodule to receive a new release', function(){
+    // Make sure we have the submodule in dist
+    run("git submodule init");
+    run("git submodule update");
+    run("cd dist; git checkout master");
+    // Bump version
+    var newVer = grunt.config('pkg').version;
+    var comp = grunt.file.readJSON(DISTDIR+"/component.json");
+    grunt.log.writeln("Package version: " + newVer);
+    grunt.log.writeln("Component version: " + comp.version);
+    if( !semver.gt( newVer, comp.version ) ){
+      grunt.warn("Need to up-version package.json first!");
+    }
+  });
+
+
+  grunt.registerTask('release-commit', 'push new build to bower component repo', function(){
+    // Stamp version
+    var newVer = grunt.config('pkg').version;
+    var comp = grunt.file.readJSON(DISTDIR+"/component.json");
+    grunt.log.writeln("Package version: " + newVer);
+    grunt.log.writeln("Component version: " + comp.version);
+    if( !semver.gt( newVer, comp.version ) ){
+      grunt.warn("Need to up-version package.json first!");
+    }
+    comp.version = newVer;
+    grunt.file.write(DISTDIR+"/component.json", JSON.stringify(comp, null, '  ')+'\n');
+    // Commit submodule
+    // Tag submodule
+    run('cd dist; git commit -a -m"Build version '+ newVer +'"', "Commited to bower repo");
+    run('cd dist; git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged bower repo");
+    // Commit and tag this.
+    run('git commit -a -m"Build version '+ newVer +'"', "Commited to source repo");
+    run('git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged source repo");
+    run("git submodule update");
+    // push?
+    grunt.log.ok("DON'T FORGET TO PUSH BOTH!");
+  });
+
+  grunt.registerTask('release', 'build and push to the bower component repo', 
+                     ['release-prepare', 'dist', 'release-commit']);
+ 
 };
