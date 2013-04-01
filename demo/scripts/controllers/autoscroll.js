b5d6a45e7d4eb52e524f5ea844cac39fab2b7f40
Paul Thomas <paul@stackfull.com>
2013-04-01 23:06:58 +0100

    push messages into the autoscroll demo.

--- demo/scripts/controllers/autoscroll.js
+++ demo/scripts/controllers/autoscroll.js
@@ -1,9 +1,15 @@
 (function(){
   'use strict';
-  angular.module('virtualScrollingApp').controller('AutoScrollCtrl', function AutoScrollCtrl($scope) {
+
+  angular.module('virtualScrollingApp').controller('AutoScrollCtrl', function AutoScrollCtrl($scope, $timeout) {
     $scope.log = {
       title: "None",
       msgs: []
     };
+    (function poll () {
+      var now = new Date();
+      $scope.log.msgs.push( { message: "Another log at " + now, time: now.getTime() } );
+      $timeout(poll, 1000);
+    })();
   });
 }());
